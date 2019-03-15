23e5062886bfb9e8baf729a0a1d11b2d83f66e2a
examin <manish.sharmaserver008@yahoo.com>
2019-03-15 13:11:19 +0530

    sds

--- src/main/java/com/sport/InsertGen.java
+++ src/main/java/com/sport/InsertGen.java
@@ -3,54 +3,92 @@ package com.sport;
 import au.com.bytecode.opencsv.CSVReader;
 
 import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author examin
  */
 public class InsertGen {
 
-    public static void main(String[] args) {
-        String filePath = "/home/examin/cc_acq_monthly_20181031_done.txt";
+    public static void main(String[] args) throws IOException, IllegalAccessException {
+        String filePath = "/home/examin/Videos/final2.csv";
         String tableName = "FACT_CC_ACQUISITION";
+
+        String schemaPath = "/home/examin/Videos/schema.sql";
+        Set<Integer> IndexOfNumericValues = getNumericIndex(schemaPath, tableName);
+
+
         final int groupedQuery = 5;
-        String firstPartLine = "INSERT INTO `" + tableName + "` ";
 
-        try {
-            File file = createFile(filePath);
-            CSVReader reader = new CSVReader(new FileReader(filePath), '|');
-            String[] currentLine;
-            int counter = 0;
-            String headerToInsert = buildTableHeader(reader);
-            FileWriter writer = new FileWriter(file);
-            BufferedWriter bf = new BufferedWriter(writer);
-            int i = 10;
-            while ( i > 0 && (currentLine = reader.readNext()) != null){
-                i--;
-                currentLine = reader.readNext();
-                String values;
-                switch (counter % groupedQuery) {
-                    case 0:
-                        values = buildValues(currentLine, true);
-                        values = firstPartLine + headerToInsert + " VALUES " + values;
-                        break;
-                    case groupedQuery - 1:
-                        values = buildValues(currentLine, true);
-                        break;
-                    default:
-                        values = buildValues(currentLine, false);
-                }
-                bf.write(values);
 
-                System.out.println(values);
+        String firstPartLine = "INSERT INTO " + tableName + " ";
+        File file = createFile(filePath);
+        CSVReader csvReader = new CSVReader(new FileReader(filePath), ',');
+
+
+        String[] currentLine;
+        int counter = 0;
+        String headerToInsert = buildTableHeader(csvReader);
+        FileWriter writer = new FileWriter(file);
+        BufferedWriter bf = new BufferedWriter(writer);
+
+        while ((currentLine = csvReader.readNext()) != null) {
+            String values;
+            switch (counter % groupedQuery) {
+                case 0:
+                    values = buildValues(currentLine, true, IndexOfNumericValues);
+                    values = firstPartLine + headerToInsert + " VALUES " + values;
+                    break;
+                case groupedQuery - 1:
+                    values = buildValues(currentLine, true, IndexOfNumericValues);
+                    break;
+                default:
+                    values = buildValues(currentLine, false, IndexOfNumericValues);
             }
-            bf.close();
-            System.out.println();
-            replaceLastChar(file.getName());
-        } catch (Exception e) {
-            e.printStackTrace();
+            bf.write(values);
 
+            System.out.print("rows processed: " + (++counter) + "\r");
         }
+        bf.close();
+        replaceLastChar(file.getName());
 
+    }
+
+    private static Set<Integer> getNumericIndex(String schemaPath, String tableName) throws IllegalAccessException {
+        Set<Integer> setOfIndex = new HashSet<Integer>();
+        try {
+            FileReader fr = new FileReader(schemaPath);
+            BufferedReader bufr = new BufferedReader(fr);
+            String line;
+            Boolean isRight = false;
+            int lineCount = 0;
+            while ((line = bufr.readLine()) != null) {
+                if (line.matches("CREATE.*" + tableName + ".*")) {
+                    isRight = true;
+                    break;
+                }
+
+            }
+            if (isRight == true) {
+                while ((line = bufr.readLine()) != null) {
+                    System.out.println(line);
+                    line = line.trim();
+                    String[] words = line.split("\\s+");
+                    if (words.length > 2) {
+                        lineCount++;
+                        if (words[1].contains("varchar") || words[1].contains("VARCHAR"))
+                            setOfIndex.add(lineCount);
+                    }
+                }
+            } else {
+                throw new IllegalAccessException();
+            }
+            bufr.close();
+        } catch (IOException io) {
+            System.exit(0);
+        }
+        return setOfIndex;
     }
 
     private static File createFile(String filePath) throws IOException {
@@ -69,7 +107,7 @@ public class InsertGen {
 
         if ((header = reader.readNext()) != null) {
             for (String title : header) {
-                headerToInsert += ("`" + title + "`,");
+                headerToInsert += (title + ",");
             }
             headerToInsert = headerToInsert.substring(0, headerToInsert.length() - 1);
             headerToInsert += ") ";
@@ -77,17 +115,22 @@ public class InsertGen {
         return headerToInsert;
     }
 
-    private static String buildValues(String[] currentLine, boolean isEnd) {
+    private static String buildValues(String[] currentLine, boolean isEnd, Set<Integer> numericValueIndex) {
         String values = "(";
-
+        int currIndex = 0;
         for (String element : currentLine) {
             String replacedElement = element.replaceAll("[\n\r]", "");
             replacedElement = replacedElement.replaceAll("'", "\\\\'");
-            values += ("'" + replacedElement + "',");
+            if (!numericValueIndex.contains(currIndex)) {
+                values += (replacedElement + ",");
+            } else {
+                values += ("'" + replacedElement + "',");
+            }
+            currIndex++;
         }
         values = values.substring(0, values.length() - 1);
         if (isEnd) {
-            values += ");\n";
+            values += ");";
         } else {
             values += "),";
         }
